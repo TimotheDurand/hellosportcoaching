4628f11a8a83f2cdbd85f9ac464ec4e594f6eb6c
DT1 <kf4hfc7hpp@privaterelay.appleid.com>
2026-03-04 19:47:48 +0100

    Add tarifs.php for pricing information

---
+++ src/tarifs.php
@@ -0,0 +1,32 @@
+<?php include("header.php"); ?>
+<?php include("menu-mobile.php"); ?>
+<main>
+    <section class="section-bg">
+        <?php include("menu.php"); ?>
+        <h1 class="h1-tarifs">Tarifs</h1>
+        <section class="section-tarifs">
+            <article class="article-tarifs article-tarifs-1">
+                <h2 class="h2-tarifs">Coaching sportif<br>individuel</h2>
+                <h3 class="h3-tarifs">60€ /heure</h3>
+                <h4 class="h4-tarifs">+15€ / pers supplémentaire</h4>
+            </article>
+            <article class="article-tarifs article-tarifs-2">
+                <h2 class="h2-tarifs">Caoching sportif<br>en entreprise</h2>
+                <h3 class="h3-tarifs">80€ /heure</h3>
+            </article>
+            <article class="article-tarifs article-tarifs-3">
+                <h2 class="h2-tarifs">Marche<br>nordique</h2>
+                <h3 class="h3-tarifs">10€ / session</h3>
+                <h3 class="h3-tarifs">90€ carte 10 sessions</h3>
+                <h4 class="h4-tarifs">*validité 5 mois</h4>
+            </article>
+            <div class="div-tarifs">
+                <h5 class="h5-tarifs">&rarr;Premier bilan OFFERT</h5>
+                <h5 class="h5-tarifs">&rarr;Matériels fournis</h5>
+            </div>
+        </section>
+    </section>
+    <?php include("section-3.php"); ?>
+</main> 
+
+<?php include("footer.php"); ?>
