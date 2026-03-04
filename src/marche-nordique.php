09eed725b5e78f40af6a705e84cff53071f66617
DT1 <kf4hfc7hpp@privaterelay.appleid.com>
2026-03-04 19:43:12 +0100

    Add marche-nordique.php with content on Nordic walking
    This file introduces a new section about 'Marche nordique' with various articles detailing its origin, benefits, and practices.

---
+++ src/marche-nordique.php
@@ -0,0 +1,42 @@
+<?php include("header.php"); ?>
+<?php include("menu-mobile.php"); ?>
+<main>
+    <section class="section-bg">
+        <?php include("menu.php"); ?>
+        <div class="pres">
+            <h1 class="name">Elodie Colet</h1>
+            <p class="tel">06 07 57 05 17</p>
+        </div>
+        <h1 class="titre-marche">Marche nordique</h1>
+        <section class="section-marche">
+            <article class="article-marche-1 article-marche">
+                <h2 class="h2-marche">Origine finlandaise</h2>
+                <p class="p-marche-white">Déjà dans les années 30, les skieurs de ski de fond finnois<br class="br-s">s'entraînaient en été à marcher avec des bâtons afin<br class="br-s">de maintenir leurs capacités physiques pour la saison suivante.<br class="br-s">On essayait alors de simuler la glisse avec des bâtons de ski.</p>
+            </article>
+            <article class="article-marche-2 article-marche">
+                <h2 class="h2-marche">Qu'est ce que c'est</h2>
+                <p class="p-marche-white">Sport de plein air qui consiste en une marche accélérée avec<br class="br-s">des bâtons de marche spécifiques.<br><br>La marche nordique, c'est bon pour le corps, le coeur et<br class="br-s">l'esprit!</p>
+            </article>
+            <article class="article-marche-3 article-marche">
+                <p class="p-marche-white-2">UN ENTRAINEMENT ET UNE PRATIQUE REGULIERE<br>DE CE SPORT PERMETTRONT D'AVOIR LES<br>RESULTATS ESCOMPTES.</p>
+            </article>
+            <article class="article-marche-4 article-marche">
+                <img src="Image/image-marche-nordique.jpg" alt="Coaching sportif personnalisé en Guyane - HelloSport" class="image-marche-nordique">
+            </article>
+            <article class="article-marche-5 article-marche">
+                <h2 class="h2-marche">Bienfaits</h2>
+                <h3 class="h3-marche"><img src="Image/icon-plus.png" alt="image-plus" class="img-marche">Sport d'endurance complet</h3>
+                <p class="p-marche-black">Faisant appel à tous les membres, la marche nordique permet de tonifier votre corps. Toutes les chaînes musculaires et articulaires de votre corps sont sollicitées. Bras, épaules, abdominaux, cuisses, fessiers, etc... soit 80% de vos muscles.<br><br><br></p>
+                <h3 class="h3-marche"><img src="Image/icon-plus.png" alt="image-plus" class="img-marche">Amélioration de la fréquence cardiaque</h3>
+                <p class="p-marche-black">La marche nordique permet également de réguler votre rythme cardiaque au même titre que le jogging. De ce fait, elle augmente de 60% votre souffle et vous aide à mieux respirer.<br><br><br></p>
+                <h3 class="h3-marche"><img src="Image/icon-plus.png" alt="image-plus" class="img-marche">Fortification osseuse</h3>
+                <p class="p-marche-black">Les mouvements effectués avec les bâtons provoquent des vibrations bénéfiques à la fortification de vos os. Elle permet de lutter efficacement contre l'ostéoporose.<br><br><br></p>
+                <h3 class="h3-marche"><img src="Image/icon-plus.png" alt="image-plus" class="img-marche">Favorise l'élimination des graisses et la perte de poids</h3>
+                <p class="p-marche-black">Durant l'effort, la dépense en énergie est de 40% supérieure à la marche traditionnelle.<br><br><br></p>
+            </article>
+        </section>
+    </section>
+    <?php include("section-3.php"); ?>
+</main> 
+
+<?php include("footer.php"); ?>
