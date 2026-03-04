6299e21dba3a1ebe495e7f7a97b383ff40038485
DT1 <kf4hfc7hpp@privaterelay.appleid.com>
2026-03-04 19:46:30 +0100

    Add sport and business section to sport-et-entreprise.php

---
+++ src/sport-et-entreprise.php
@@ -0,0 +1,35 @@
+<?php include("header.php"); ?>
+<?php include("menu-mobile.php"); ?>
+<main>
+    <section class="section-bg">
+        <?php include("menu.php"); ?>
+        <div class="pres">
+            <h1 class="name">Elodie Colet</h1>
+            <p class="tel">06 07 57 05 17</p>
+        </div>
+        <section class="section-sport">
+            <article class="article-sport-1 article-sport">
+                <h1 class="h1-sport-spe">Sport et entreprise</h1>
+                <p class="p-sport-black">Vous souhaitez donner la possibilité à vos employés de faire du sport sur leur lieu de travail ?<br>Le sport en entreprise permet de lutter contre le stress, favorise la cohésion, agit sur la compétitivité,<br>augmente la motivation, favorise le bien-être et la santé des salariés.</p>
+            </article>
+            <article class="article-sport-2 article-sport">
+                <h1 class="h1-sport">Quelques chiffres</h1>
+                <p class="p-sport-orange">12% de gain de productivité pour le salarié pratiquant au moins 30 minutes d'activité physique par jour par rapport aux collègues sédentaires (étude Santé Canada).<br><br>7 à 9% d'économies réalisées par l'entreprise sur les frais de santé annuels d'un employé si celui-ci à une activité physique régulière (source : Goodwill).<br><br>3 ans d'espérance de vie gagnés en moyenne pour la pratique régulière d'une activité physique, repoussant ainsi de six ans l'âge où il devient dépendant (source: Goodwill).</p>
+            </article>
+            <article class="article-sport-3 article-sport">
+                <h1 class="h1-sport">Vous pensez ne pas avoir de place ?</h1>
+                <p class="p-sport">Une salle de réunion pourra très bien se transformer<br class="br-s">en zone sportive un jour de la semaine entre 12 et 14h<br class="br-s">ou un soir entre 18 et 19h.</p>
+                <h1 class="h1-sport">Normes d'affichages et d'organisation</h1>
+                <p class="p-sport">Vous devez afficher le tableau d’organisation des<br class="br-s">secours avec numéros et adresses des personnes et<br class="br-s">organismes susceptibles d’intervenir en cas<br class="br-s">d’urgence, une copie de l’attestation du contrat<br class="br-s">d’assurances de responsabilité civile. Trousse de<br class="br-s">secours à proximité.</p>
+                <h1 class="h1-sport">Assurances</h1>
+                <p class="p-sport">Vous devez prévenir votre assurance pour la<br class="br-s">responsabilité civile.</p>
+            </article>
+            <article class="article-sport-4 article-sport">
+                <p class="p-sport-black-2">En proposant du sport à leurs salariés, les entreprises améliorent leur image, ce qui augmente également leur<br class="br-s">attractivité pour favoriser le recrutement des jeunes (- de 30 ans). Les entreprises qui offrent un cadre de travail<br class="br-s">agréable et sportif se différencient des autres entreprises et fidélisent leurs salariés en renforçant leur attachement à<br class="br-s">l'entreprise !</p>
+            </article>
+        </section>
+    </section>
+    <?php include("section-3.php"); ?>
+</main> 
+
+<?php include("footer.php"); ?>
