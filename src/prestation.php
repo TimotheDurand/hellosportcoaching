fbd651864b05c85c354fedadb7010faba22d5f97
DT1 <kf4hfc7hpp@privaterelay.appleid.com>
2026-03-04 19:44:42 +0100

    Create prestation.php

---
+++ src/prestation.php
@@ -0,0 +1,48 @@
+<?php include("header.php"); ?>
+<?php include("menu-mobile.php"); ?>
+<main>
+    <section class="section-bg">
+        <?php include("menu.php"); ?>
+        <div class="pres">
+            <h1 class="name">Elodie Colet</h1>
+            <p class="tel">06 07 57 05 17</p>
+        </div>
+        <section class="section-pres">
+            <article class="pres-items-1 article-pres">
+                <h1 class="h1-pres"><img src="Image/icon-pres-1.png" alt="icon-pres-1" class="img-pres-icon">Perte de poids<br>remise en forme</h1>
+                <p class="p-pres">Accompagnement selon vos objectifs, au moyen de<br class="br-s">séances et matériels variés. Indissociable d'une<br class="br-s">bonne alimentation.</p>
+            </article>
+            <article class="pres-items-2 article-pres">
+                <h1 class="h1-pres"><img src="Image/icon-pres-2.png" alt="icon-pres-2" class="img-pres-icon">Musculation</h1>
+                <p class="p-pres">Augmenter son métabolisme de base, prendre de<br class="br-s">la masse musculaire, dessiner ses muscles au  moyen<br class="br-s">de sangles de suspension, kettlebell, poids du corps,<br class="br-s">haltères, barre olympique, etc...</p>
+            </article>
+            <article class="pres-items-3 article-pres">
+                <h1 class="h1-pres"><img src="Image/icon-pres-3.png" alt="icon-pres-3" class="img-pres-icon">Pilates / Posture</h1>
+                <p class="p-pres">Avoir une meilleure conscience de son corps, mieux<br class="br-s">se tenir, travailler ses muscles profonds. Sportif ou<br class="br-s">non, jeune ou moins jeune, cette pratique est ouverte<br class="br-s">à tous!</p>
+            </article>
+            <article class="pres-items-4 article-pres">
+                <h1 class="h1-pres"><img src="Image/icon-pres-4.png" alt="icon-pres-4" class="img-pres-icon">Circuit training / Interval<br>training</h1>
+                <p class="p-pres">Travailler son endurance, se muscler, brûler les<br class="br-s">graisses en y mettant de l'intensité.</p>
+            </article>
+            <article class="pres-items-5 article-pres">
+                <h1 class="h1-pres"><img src="Image/icon-pres-5.png" alt="icon-pres-5" class="img-pres-icon">Sport et grossesse</h1>
+                <p class="p-pres">Création et encadrement de séances adaptées pré-<br class="br-s">natales et post-natales.<br class="br-s">Se mouvoir en douceur avant et après l'arrivée de<br class="br-s">bébé est une nécessité pour une grossesse et une<br class="br-s">maternité sereines.</p>
+            </article>
+            <article class="pres-items-6 article-pres">
+                <h1 class="h1-pres"><img src="Image/icon-pres-6.png" alt="icon-pres-6" class="img-pres-icon">Sport et handicap</h1>
+                <p class="p-pres">Travaille auprès d'adultes en situation de handicap<br class="br-s">dont les capacités à réaliser les actes de la vie<br class="br-s">courante sont altérées. Pathologie neurologique<br class="br-s">invalidante, troubles psychiques, troubles du spectre<br class="br-s">autistique, déficiences motrices et/ou intellectuelles<br class="br-s">liées  à un accident, une maladie ou de naissance.</p>
+            </article>
+            <article class="pres-items-7 article-pres">
+                <h1 class="h1-pres"><img src="Image/icon-pres-7.png" alt="icon-pres-7" class="img-pres-icon">Stretching / Mobilité</h1>
+                <p class="p-pres">Libérer le corps de ses tensions physique et<br class="br-s">psychique. Rééquilibrer le tonus musculaire,<br class="br-s">retrouver de la fluidité et de la souplesse dans ses<br class="br-s">mouvements.</p>
+            </article>
+            <article class="pres-items-8 article-pres">
+                <h1 class="h1-pres"><img src="Image/icon-pres-8.png" alt="icon-pres-8" class="img-pres-icon">Equilibre Alimentaire<br>conseils nutrition</h1>
+                <p class="p-pres">Reprendre le contrôle de sa santé par une<br class="br-s">alimentation saine et équilibrée. Sans privation, sans<br class="br-s">frustration. C'est possible!</p>
+            </article>
+        </section>
+    </section>
+    <?php include("section-3.php"); ?>
+</main>
+
+<?php include("footer.php"); ?>
